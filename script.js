95a5bf1abab4d62044b27337a34269d6ae7c96b4
sinatra27 <147604618+sinatra27@users.noreply.github.com>
2024-06-02 23:54:56 -0400

    Add read/unread and remove book features

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// Initialize empty array to store new books
 const myLibrary = [];
-const showDialogBtn = document.querySelector('.show-dialog');
+
+// Select HTML elements
+const addDialogBtn = document.querySelector('.add-dialog');
 const dialog = document.querySelector('.dialog');
 const form = document.querySelector('.book-form');
 const closeDialog = document.querySelector('.close-dialog');
@@ -8,41 +11,64 @@ const newBookTitle = document.querySelector('#title');
 const newBookAuthor = document.querySelector('#author');
 const newBookPages = document.querySelector('#pages');
 const newBookRead = document.querySelector('#read');
-const container = document.querySelector('.library');
+const library = document.querySelector('.library');
 
-// Object constructor
+// Object constructor for new books
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`;
+        // return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+        return `${this.title}<br>by ${this.author}<br>${this.pages} pages<br>`;
     };
 }
 
-// Function accepts the user's inputs and stores new book object in library array
+// Accepts user's inputs and stores new book object in myLibrary array
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
     displayBooks();
 }
 
-// Function loops through the array and displays each book on the page
+// Loop through myLibrary array and display each book on page
 function displayBooks() {
-    container.replaceChildren();
+    library.replaceChildren();
     for (let i = 0; i < myLibrary.length; i++) {
+        // Display book details
         const displayBook = document.createElement('div');
         displayBook.classList.add('books');
-        container.appendChild(displayBook);
+        library.appendChild(displayBook);
         const bookInfo = document.createElement('p');
         bookInfo.innerHTML = myLibrary[i].info();
         displayBook.appendChild(bookInfo);
+        // Read/not read button
+        const readBtn = document.createElement('button');
+        readBtn.classList.add('read-btn');
+        readBtn.setAttribute('data-book', i);
+        if (myLibrary[i].read == true) {
+            readBtn.classList.add('read');
+            readBtn.innerHTML = 'Read';
+        }
+        else {
+            readBtn.classList.add('not-read');
+            readBtn.innerHTML = 'Not Read';
+        }
+        readBtn.onclick = toggleRead;
+        displayBook.appendChild(readBtn);
+        // Remove book button
+        const removeBtn = document.createElement('button');
+        removeBtn.classList.add('remove-btn');
+        removeBtn.setAttribute('data-book', i);
+        removeBtn.innerHTML = 'Remove';
+        removeBtn.onclick = removeBook;
+        displayBook.appendChild(removeBtn);
     }
 }
 
 // Dialog for adding new book details
-showDialogBtn.addEventListener('click', () => {
+addDialogBtn.addEventListener('click', () => {
     dialog.showModal();
 });
 closeDialog.addEventListener('click', (e) => {
@@ -51,21 +77,38 @@ closeDialog.addEventListener('click', (e) => {
 });
 addNewBook.addEventListener('click', (e) => {
     e.preventDefault();
-    if (newBookRead.checked == true) newBookRead.value = 'read';
-    else newBookRead.value = 'not read yet';
-    // console.log(newBookTitle.value, newBookAuthor.value, newBookPages.value, newBookRead.value);
-    addBookToLibrary(newBookTitle.value, newBookAuthor.value, newBookPages.value, newBookRead.value);
+    addBookToLibrary(newBookTitle.value, newBookAuthor.value, newBookPages.value, newBookRead.checked);
     dialog.close();
     form.reset();
 });
 
+// Toggle read/unread
+const toggleRead = (e) => {
+    const bookIndex = e.target.getAttribute('data-book');
+    myLibrary[bookIndex].read = !myLibrary[bookIndex].read;
+    displayBooks();
+};
+
+// Remove book from library
+const removeBook = (e) => {
+    const bookIndex = e.target.getAttribute('data-book');
+    myLibrary.splice(bookIndex, 1);
+    displayBooks();
+};
+
+
+
+
+
+
 // const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
 // console.log(theHobbit.info());
 
-
-
 /* 
 To-do:
-- fn that displays each book on page
+- add read checkmark image?
+- change btn color of read/not read
+- form fields -valid/invalid/required
+    - mdn form validation
 
 */
